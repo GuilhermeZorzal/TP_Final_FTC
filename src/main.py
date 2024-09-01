e54028adadc4ab4b02e734c170b96f6c8cf29d39
Maria Eduarda de Pinho Braga <mpbduda@gmail.com>
2024-09-01 17:24:32 -0300

    add mealy

--- src/main.py
+++ src/main.py
@@ -3,31 +3,13 @@ import alfabeto as s
 import leitura as l
 import os
 import platform
+import mealy as m
 
-
-def main():
-    if platform.system() == "Windows":
-        os.system('cls')
-    else:
-        os.system('clear')
-    
+def run_case1(sigma):
     nome_receita = input("Insira o nome da receita desejada (sem .txt)\n>> ")
-    
     dir = "pocoes/"
-    arq_receita = dir + nome_receita + ".txt"
-    arq_ingredientes = dir + "ingredientes.txt"
-    arq_reacoes = dir + "reacoes.txt"
-
-    try:
-        # Criação do alfabeto pode falhar, se um dos arquivos não existir.
-        # Não tenho certeza se essa foi a melhor forma de arquitetar esse
-        # comportamento; inicializadores que leem arquivos e lançam exceções
-        # são um pouco estranhos. Mas funciona
-        sigma = s.Alfabeto(arq_ingredientes, arq_reacoes)
-    except Exception as e:
-        print(f"> alfabeto.py: [!] Erro ao criar o alfabeto. Erro: {e}")
-        exit(1)
-
+    arq_receita =  dir + nome_receita + ".txt"
+    
     receita = l.carrega_receita(arq_receita, sigma)
     if receita is not None:
         print(f"Receita lida do arquivo {dir}{arq_receita}\n")
@@ -39,6 +21,7 @@ def main():
     # Criação de poção
     primeiro = True
     auto = a.Automato(receita)
+    
     while True:
         if primeiro:
             ing = input("Insira o símbolo do primeiro ingrediente: ")
@@ -58,6 +41,35 @@ def main():
     nome_receita = nome_receita.replace("_", " ").capitalize()
     print()
     print(f"{nome_receita} criada" if auto.reconheceu() else "Falha na mistura")
+
+def main():
+    if platform.system() == "Windows":
+        os.system('cls')
+    else:
+        os.system('clear')
+        
+    resp = int(input("Qual tipo de máquina você deseja usar?\n1 - Automato Determinístico(tipo é determinado pela entrada)\n2 - Máquina de Mealy \n>> "))
+
+    dir = "pocoes/"
+    arq_ingredientes = dir + "ingredientes.txt"
+    arq_reacoes = dir + "reacoes.txt"
+
+    try:
+        # Criação do alfabeto pode falhar, se um dos arquivos não existir.
+        # Não tenho certeza se essa foi a melhor forma de arquitetar esse
+        # comportamento; inicializadores que leem arquivos e lançam exceções
+        # são um pouco estranhos. Mas funciona
+        sigma = s.Alfabeto(arq_ingredientes, arq_reacoes)
+    except Exception as e:
+        print(f"> alfabeto.py: [!] Erro ao criar o alfabeto. Erro: {e}")
+        exit(1)
+
+    match resp:
+        case 1:
+            run_case1(sigma)
+        case 2:
+            mealy = m.Mealy()
+            mealy.run(sigma)
 
 # Essa condicional irá executar sempre que esse arquivo for executado
 # diretamente. Quando ele for incluído como uma biblioteca (no REPL, por
