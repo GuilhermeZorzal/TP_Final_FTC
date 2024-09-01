9c37e046039fb301723279c49985b4d2cd968772
Guilherme Broedel Zorzal <guilhermezorzal2004@gmail.com>
2024-08-31 22:09:53 -0300

    mealy

--- src/main.py
+++ src/main.py
@@ -12,10 +12,9 @@ def main():
     else:
         os.system('clear')
     
-    nome_receita = input("Insira o nome da receita desejada\n>> ")
-    
+    resp = int(input("Qual tipo de máquina você deseja usar?\n1 - Automato Determinístico(tipo é determinado pela entrada)\n2 - Máquina de Mealy \n>> "))
     dir = "../poções/"
-    arq_receita = dir + nome_receita + ".txt"
+
     arq_ingredientes = dir + "ingredientes.txt"
     arq_reacoes = dir + "reações.txt"
 
@@ -27,28 +26,26 @@ def main():
         sigma = s.Alfabeto(arq_ingredientes, arq_reacoes)
     except: exit(1)
 
-    receita = l.carrega_receita(arq_receita, sigma)
-    if receita is not None:
-        print(f"Receita lida do arquivo {arq_receita}\n")
-        if(input("Deseja ver a receita? [s/n]\n>> ") == 's'):
-            receita.imprime()
-        
-    if(input("Deseja ver os ingredientes disponíveis? [s/n]\n>> ") == 's'):
-            sigma.lista_ingredientes()
-    
-    # Executa o automato, realizando as transições
-
-    resp = int(input("Qual tipo de máquina você deseja usar?\n1 - Automato Determinístico(tipo é determinado pela entrada)\n2 - Máquina de Mealy \n>> "))
-
     match resp:
         case 1:
-            automato = a.Automato(receita)  
-            automato.run(sigma, nome_receita)
-        case 2:
-            mealy = m.Mealy(receita)
-            mealy.run(sigma)
+            nome_receita = input("Insira o nome da receita desejada\n>> ")
+            arq_receita = dir + nome_receita + ".txt"
 
-    automato.run(sigma, nome_receita)
+            receita = l.carrega_receita(arq_receita, sigma)
+            if receita is not None:
+                print(f"Receita lida do arquivo {arq_receita}\n")
+                if(input("Deseja ver a receita? [s/n]\n>> ") == 's'):
+                    receita.imprime()
+                
+            if(input("Deseja ver os ingredientes disponíveis? [s/n]\n>> ") == 's'):
+                    sigma.lista_ingredientes()
+                    automato = a.Automato(receita)  
+                    automato.run(sigma, nome_receita)
+        case 2:
+            mealy = m.Mealy()
+            mealy.run(sigma)
+    
+
 
 
 # Essa condicional irá executar sempre que esse arquivo for executado
--- src/mealy.py
+++ src/mealy.py
@@ -1,18 +1,22 @@
 from receita import Receita
 from alfabeto import Alfabeto
-
+import time
+import os
+import platform
 # Realização dinâmica de um diagrama de estados
 class Mealy:
-    def __init__(self, receita: Receita):
-        self.estado_atual = receita.inicial
+    def __init__(self):
         self.descricoes = dict()
         self.le_mealy()
 
     def run(self, sigma : Alfabeto):
         primeiro = True
-
+        cont = 0
+        poder = 0
+        sabor = 0
         while True:
             # Recebe ingrediente
+            cont+=1
             if primeiro:
                 ing = input("Insira o símbolo do primeiro ingrediente: ")
                 if not sigma.valida_ingrediente(ing):
@@ -27,7 +31,30 @@ class Mealy:
                 if not sigma.valida_ingrediente(ing):
                     print("Ingrediente não reconhecido...")
                     continue
-            print(self.descricoes[ing])
+            # print(self.descricoes[ing])
+            sabor += int(self.descricoes[ing][1])
+            poder += int(self.descricoes[ing][2])                
+        if platform.system() == "Windows":
+            os.system('cls')
+        else:
+            os.system('clear')
+        for i in range(4):
+            print("O corvo provador pegou uma colherada e está avaliado o sabor" + "." * i)
+            time.sleep(0.5)
+            if platform.system() == "Windows":
+                os.system('cls')
+            else:
+                os.system('clear')
+        
+        if sabor < 0:
+            print("O corvo desmaiou defido ao gosto terrível da poção")
+            return
+        if cont > 10:
+            print("O corvo gralhou, disse que a poção ficou muito misturada e nao conseguiu provar direito")
+            return        
+        self.avalia_sabor(sabor)
+        self.avalia_poder(poder)
+        return
 
     
     def le_mealy(self):
@@ -35,12 +62,50 @@ class Mealy:
             for line in arq:
                 ing, descricao = line.split(':')
                 ing = ing.strip()
-                descricao = descricao.strip()
+                descricao = descricao.split("|")
+                for i in descricao:
+                    i = i.strip()
                 self.descricoes.update({ing : descricao})
     def imprime_descricoes(self):
         for ing in self.descricoes:
-            print(f"{ing}: {self.descricoes[ing]}")
+            print(f"{ing}: {self.descricoes[ing][0]}")
         
+    def avalia_sabor(self,sabor):
+        if sabor == 0:
+            print("O corvo provador disse que sua poção ficou aguada")
+            return
+        elif sabor < 10:
+            print("O corvo provador disse que sua pocao ficou meio sem graça")
+            return
+        elif sabor < 20:
+            print("O corvo provador disse que sua pocao ficou muito boa")
+            return
+        elif sabor < 40:
+            print("O corvo provador disse que sua pocao ficou espetacular")
+            return
+        elif sabor >= 60:
+            print("O corvo provador disse que voce deveria largar a bruxaria e virar chefe de cozinha")
+            return
+    def avalia_poder(self,poder):
+        if poder < 0:
+            print("O corvo provador desmaiou de fraqueza")
+            return
+        elif poder == 0:
+            print("O corvo provador disse que sua pocao nao tem poder algum")
+            return
+        elif poder < 50:
+            print("O corvo provador disse que sua pocao tem um poder mediano")
+            return
+        elif poder < 100:
+            print("O corvo provador disse que sua pocao é bastante poderosa")
+            return
+        elif poder < 200:
+            print("O corvo provador disse que sua pocao é extremamente poderosa!!!")
+            return
+        elif poder >= 400:
+            print("O pocao é muito poderosa. O poder do corvo provador ultrapassa seu próprio poder! ele pega o caldeirão com as próprias garras e sai voando com ele pela janela!!!")
+            return
+
 
 
         
